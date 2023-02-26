55f9a64aeaecf793fea7e3e31fb317977833163d
dellabeneta <m.dellabeneta@gmail.com>
2023-02-26 19:22:01 -0300

    .

--- ssh_key.tf
+++ ssh_key.tf
@@ -1,5 +1,5 @@
 # Create a new SSH key
 resource "digitalocean_ssh_key" "ssh_key" {
   name       = "kubernetes"
-  public_key = file("C:/Users/miche/.ssh/id_rsa.pub")
+  public_key = file("~/.ssh/id_rsa.pub")
 }
